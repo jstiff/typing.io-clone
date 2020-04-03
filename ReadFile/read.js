a0f76f64e9b022a9d868ccedb6e362524422ac76
Jason Stiff <jasonstiff@Jasons-MacBook-Pro.local>
2020-04-03 13:06:43 -0500

    read.js sort of works

--- ReadFile/read.js
+++ ReadFile/read.js
@@ -1,18 +1,36 @@
 const fs = require('fs');
 
-let data= fs.readFileSync('../sourceCode.txt', 'utf-8');
-let formatted= data.split("");
-
-
-for(let i =0; i<formatted.length; i++){
-  fs.writeFile('source.html',`<span>${formatted[i]}</span>` , err=> {
-    if(err){
-      console.error(err)
-      return
-    }
-  })
-
+//let data= fs.readFileSync('../sourceCode.txt', 'utf-8');
+//let formatted= data.split("");
+var data;
+var formatted;
+try{
+   data= fs.readFileSync('../sourceCode.txt', 'utf-8');
+   formatted= data.split("");
+   formatted.forEach(i=>{fs.appendFileSync('source.html',`<span>${i}</span>`)})
+}catch(err){
+  console.log("Error occured while trying to read the file");
 }
+
+// try{
+//   fs.writeFileSync('source.html', formatted)
+// }catch(err){
+//   console.log("error occured while writing to file");
+// }
+
+
+
+
+
+// for(let i =0; i<formatted.length; i++){
+//   fs.writeFile('source.html',`<span>${formatted[i]}</span>` , err=> {
+//     if(err){
+//       console.error(err)
+//       return
+//     }
+//   })
+//
+// }
 
 //console.log(poop);
 //formatted.forEach((i) =>{
