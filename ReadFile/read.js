f35d47a879d1f4ac20ed5ef56514dce90d15fd2d
Jason Stiff <jasonstiff@Jasons-MacBook-Pro.local>
2020-04-04 12:14:59 -0500

    reformatted code with  prettier

--- ReadFile/read.js
+++ ReadFile/read.js
@@ -1,29 +1,23 @@
-const fs = require('fs');
-
+const fs = require("fs");
 
 let data;
 let formatted;
 
-try{
-   data= fs.readFileSync('../sourceCode.txt', 'utf-8');
-   formatted= data.split("");
-   console.log(formatted[3]==='\s')
-   //fs.writeFileSync('log.txt', formatted)
-   formatted.forEach(i=>{
-     //if(i==='\t'||'\s'){console.log('Tab found!')}
-     if(i==='\n'){fs.appendFileSync('source.html', `<br />`)}
-     fs.appendFileSync('source.html',`<span>${i}</span>`)})
-}catch(err){
+try {
+  data = fs.readFileSync("../sourceCode.txt", "utf-8");
+  formatted = data.split("");
+  console.log(formatted[3] === "s");
+  //fs.writeFileSync('log.txt', formatted)
+  formatted.forEach(i => {
+    //if(i==='\t'||'\s'){console.log('Tab found!')}
+    if (i === "\n") {
+      fs.appendFileSync("source.html", `<br />`);
+    }
+    fs.appendFileSync("source.html", `<span>${i}</span>`);
+  });
+} catch (err) {
   console.log("Error occured while trying to read the file");
 }
-
-
-
-
-
-
-
-
 
 // for(let i =0; i<formatted.length; i++){
 //   fs.writeFile('source.html',`<span>${formatted[i]}</span>` , err=> {
--- index.js
+++ index.js
@@ -1,50 +1,44 @@
-
 const btnGitHub = document.getElementById("btn2");
 const code = document.getElementsByClassName("mainContainer")[0];
 btnGitHub.addEventListener("click", gitHubFetch);
 
-async function gitHubFetch(){
-	const url = "https://api.github.com/repos/substack/node-falafel/contents/index.js"
-	const response = await fetch(url);
-	const result = await response.json();
-	console.log(result);
-	const anchor = document.createElement("a");
-	anchor.href= result.download_url;
-	anchor.textContent = result.name;
-	code.appendChild(anchor);
+async function gitHubFetch() {
+  const url =
+    "https://api.github.com/repos/substack/node-falafel/contents/index.js";
+  const response = await fetch(url);
+  const result = await response.json();
+  console.log(result);
+  const anchor = document.createElement("a");
+  anchor.href = result.download_url;
+  anchor.textContent = result.name;
+  code.appendChild(anchor);
 
-	code.appendChild(document.createElement("br"))
-
-};
-
-
-const spanChar = document.querySelector('span');
-const characters = document.querySelectorAll('span');
-
-
-console.log(characters.length);
-characters.forEach(spanChar =>{
-	console.log(spanChar.innerText)
-})
-
-
-let i = 0;
-function keyHandler(eventData){
-	const currentChar = characters[i]
-	if(currentChar.innerText==eventData.key){
-		i+=1;
-		console.log("SUCCESS!!!" + " " + currentChar.innerText);
-		currentChar.setAttribute("class", "correct");    //setAttribute just overides the current class
-		//currentChar.classList.add("correct");
-		//currentChar.classList.remove("wrong");
-	}else{
-		console.log("falied!!!");
-		currentChar.setAttribute("class", "wrong");
-		//currentChar.classList.add("wrong");
-		//currentChar.classList.remove("correct");
-		
-	}
+  code.appendChild(document.createElement("br"));
 }
 
+const spanChar = document.querySelector("span");
+const characters = document.querySelectorAll("span");
 
-document.addEventListener('keydown', keyHandler);
+console.log(characters.length);
+characters.forEach(spanChar => {
+  console.log(spanChar.innerText);
+});
+
+let i = 0;
+function keyHandler(eventData) {
+  const currentChar = characters[i];
+  if (currentChar.innerText == eventData.key) {
+    i += 1;
+    console.log("SUCCESS!!!" + " " + currentChar.innerText);
+    currentChar.setAttribute("class", "correct"); //setAttribute just overides the current class
+    //currentChar.classList.add("correct");
+    //currentChar.classList.remove("wrong");
+  } else {
+    console.log("falied!!!");
+    currentChar.setAttribute("class", "wrong");
+    //currentChar.classList.add("wrong");
+    //currentChar.classList.remove("correct");
+  }
+}
+
+document.addEventListener("keydown", keyHandler);
